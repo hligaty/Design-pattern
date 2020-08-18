f63fdd6b6889e1cfdb82edc3556521f90378c637
hligy <1970316213@qq.com>
2020-08-18 17:09:02 +0800

    装饰模式

--- src/com/example/chapter17/unit1/Main.java
+++ src/com/example/chapter17/unit1/Main.java
@@ -6,6 +6,8 @@ public class Main {
      * 装饰模式
      * 定义：动态地给一个对象添加一些额外的职责。
      * 注意：下面的嵌套说明了复杂性，因此要少些装饰。
+     *
+     * 可以扩展父类功能，避免直接修改父类影响子类
      */
     SchoolReport report;
     report = new FouthGradeSchoolReport();
