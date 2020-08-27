7b476b49698a58c2fbaeabe12c24ee078b74f69b
hligy <1970316213@qq.com>
2020-08-27 17:15:14 +0800

    抽象方法模式

--- src/com/example/chapter9/unit1/Main.java
+++ src/com/example/chapter9/unit1/Main.java
@@ -2,9 +2,9 @@ package com.example.chapter9.unit1;
 
 public class Main {
   public static void main(String[] args) {
-    /*
-    抽象方法模式
-    定义：为创建一组相关或相互依赖的对象提供一个接口，而且无须指定它们的具体类。
+    /**
+     * 抽象方法模式
+     * 定义：为创建一组相关或相互依赖的对象提供一个接口，而且无须指定它们的具体类。
      */
     HumanFactory maleHumanFactory = new MaleHumanFactory();
     HumanFactory feMaleHumanFactory = new FemaleHumanFactory();
