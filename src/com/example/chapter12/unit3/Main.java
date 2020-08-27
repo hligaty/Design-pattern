0a09c62f57b0b56478774258624f386c9591c3da
hligy <1970316213@qq.com>
2020-08-27 17:25:16 +0800

    代理模式，动态代理

--- src/com/example/chapter12/unit3/Main.java
+++ src/com/example/chapter12/unit3/Main.java
@@ -2,9 +2,9 @@ package com.example.chapter12.unit3;
 
 public class Main {
   public static void main(String[] args) {
-    /*
-    代理模式，动态代理
-    应用：AOP，切面（Aspect）、切入点（JoinPoint）、通知（Advice）、织入（Weave）
+    /**
+     * 代理模式，动态代理
+     * 应用：AOP，切面（Aspect）、切入点（JoinPoint）、通知（Advice）、织入（Weave）
      */
   }
 }
