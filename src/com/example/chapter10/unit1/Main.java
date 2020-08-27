66f3ce73b2b9e265f509f4ccc9eeba712a26a0a1
hligy <1970316213@qq.com>
2020-08-27 17:15:41 +0800

    模板方法模式

--- src/com/example/chapter10/unit1/Main.java
+++ src/com/example/chapter10/unit1/Main.java
@@ -3,9 +3,9 @@ package com.example.chapter10.unit1;
 public class Main {
 
   public static void main(String[] args) {
-    /*
-    模板方法模式
-    定义：定义一个操作中的算法的框架，而将一些步骤延迟到子类中。使得子类可以不改变一个算法的结构即可重定义该算法的某些特定步骤。
+    /**
+     * 模板方法模式
+     * 定义：定义一个操作中的算法的框架，而将一些步骤延迟到子类中。使得子类可以不改变一个算法的结构即可重定义该算法的某些特定步骤。
      */
     HummerH1Model h1 = new HummerH1Model();
     HummerH2Model h2 = new HummerH2Model();
