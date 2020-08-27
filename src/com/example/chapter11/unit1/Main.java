0cb3ed8acdbc6519227e365b79c980bd97ac40b6
hligy <1970316213@qq.com>
2020-08-27 17:16:11 +0800

    建造者模式

--- src/com/example/chapter11/unit1/Main.java
+++ src/com/example/chapter11/unit1/Main.java
@@ -3,9 +3,9 @@ package com.example.chapter11.unit1;
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
-    /*
-    建造者模式
-    定义：一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
+    /**
+     * 建造者模式
+     * 定义：一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
      */
     Director director = new Director();
     //一万辆 A 型奔驰
