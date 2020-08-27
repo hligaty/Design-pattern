7e5ce5ea7c6d309b01c0e9bc146966f857e5fc17
hligy <1970316213@qq.com>
2020-08-27 17:13:59 +0800

    工厂模式

--- src/com/example/chapter8/unit1/Main.java
+++ src/com/example/chapter8/unit1/Main.java
@@ -3,9 +3,9 @@ package com.example.chapter8.unit1;
 public class Main {
 
   public static void main(String[] args) {
-    /*
-    工厂方法模式
-    定义：定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类。
+    /**
+     * 工厂模式
+     * 定义：定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类。
      */
     AbstractHumanFactory humanFactory = new HumanFactory();
     Human blackHuman = humanFactory.createHuman(BlackHuman.class);
